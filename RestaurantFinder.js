6b3ee7721bc40767911f88336206963df1d29cb0
Shindepoonam <116255804+Shindepoonam@users.noreply.github.com>
2022-10-25 12:59:25 +0530

    Add files via upload

---
+++ RestaurantFinder.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL : "https://github.com/Shindepoonam/Restaurant-finder" ,
+});
+"https://github.com/Shindepoonam/Restaurant-finder";

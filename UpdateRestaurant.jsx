109c44d6b065ef97c4366b8123f0bb6ccf91b3bc
Shindepoonam <116255804+Shindepoonam@users.noreply.github.com>
2022-10-25 17:34:16 +0530

    Add files via upload

---
+++ UpdateRestaurant.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams }from "react-router-dom";
+
+const UpdateRestaurant = (props) => {
+    const{id} =useParams();
+    console.log(test);
+    return <div></div>;
+};
+
+export default UpdateRestaurant;
